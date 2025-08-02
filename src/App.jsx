b3da761f6621c11e3bf4492ac6f30febe5ad5af4
reireipien <reiton20040905@gmail.com>
2025-08-02 17:36:19 +0900

    ハローにした

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-// src/App.jsx
-import Calculator from "./components/Calculator";
-
 function App() {
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>React + Vite 電卓</h1>
-      <Calculator />
+    <div>
+      <h1>Hello, world!</h1>
     </div>
   );
 }
